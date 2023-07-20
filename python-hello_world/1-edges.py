bb4d0e67b2b30ba5ae014cf3531394bdcbb6d91c
chimsoma <ndechimsoma@gmail.com>
2023-07-20 03:40:12 +0100

    edgey

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberto"
-word_first_3 = "Sch"
+word_first_3 = "Hol"
 print("First 3 letters: {}" .format(word_first_3))
-word_last_2 = "ol"
+word_last_2 = "on"
 print("Last 2 letters: {}" .format(word_last_2))
-middle_word = "choo"
+middle_word = "olberto"
 print("Middle word: {}" .format(middle_word))
