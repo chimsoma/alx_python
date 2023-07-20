468e86ec37498b91264d011d20d469c4826523ab
chimsoma <ndechimsoma@gmail.com>
2023-07-20 02:00:59 +0100

    My commit

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-print(number)
+if number < 0:
+    is_negetive = number -10
+    print("number {} is negetive" .format(number))
+
+else:
+    is_positive = number % 10
+    print("number {} is positive" .format(number))
+if number == 0:
+    print("number {} is zero" .format(number))
