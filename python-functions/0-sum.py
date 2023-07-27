4e56028209c233960b34733e5b6e0c0bccba0f41
chimsoma <ndechimsoma@gmail.com>
2023-07-27 18:41:37 +0100

    adding two integers

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,11 +1,4 @@
 #A function that adds two integers and returns the result.
 #!/usr/bin/env python3
-a = 1
-b = 2
-c =  98
-d = 0
-e = 100
-f = -2
-print(a + b)
-print(c + d)
-print(e + f)
+def add(a, b):
+    return a + b 
