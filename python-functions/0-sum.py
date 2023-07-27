2d02b2b4efc4f1cf12a5bea70458511f55d218e0
chimsoma <ndechimsoma@gmail.com>
2023-07-27 01:09:09 +0100

    My first commit

---
+++ python-functions/0-sum.py
@@ -0,0 +1,11 @@
+#A function that adds two integers and returns the result.
+#!/usr/bin/env python3
+a = 1
+b = 2
+c =  98
+d = 0
+e = 100
+f = -2
+print(a + b)
+print(c + d)
+print(e + f)
---
+++ python-functions/README.md
@@ -0,0 +1,1 @@
+using python functions for arithmetic purpose
