82068fd592885a5c1faab387e7d7a6b0e809f74b
chimsoma <ndechimsoma@gmail.com>
2023-08-06 16:19:53 +0100

    size validation

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+"""
+Updating the size value to an integer and also raising a type and value error 
+to validate that size value is an integer before assigning size to the private instance attribute.
+"""
+class Square:
+    def __init__(self, size = 0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >=0")
+        else:
+            self.__size = size
