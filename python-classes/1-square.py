d8edffa7c6a541a13e97d8c516e387a70a57ddf9
chimsoma <ndechimsoma@gmail.com>
2023-08-07 00:50:20 +0100

    commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -7,10 +7,20 @@ class Square:
     The attribute is made private so, the class builder (author) can easily control the type and value of this attribute.
     """
     def __init__(self, size):
-        def __init__(self, size =0):
-            if type(size) is not int:
-                raise TypeError("size must be an integer")
-            elif size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
+        """
+classes project;
+"""
+class Square:
+ """
+Updating the class function, size and its value to an integer and also raising a type and value error 
+to validate that size value is an integer before assigning size to the private instance attribute.
+"""
+def __init__(self, size =0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+        self.__size = size 
