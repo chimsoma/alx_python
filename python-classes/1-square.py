95ec179e4b4e79f2a15bc8378ba553c55697871b
chimsoma <ndechimsoma@gmail.com>
2023-08-06 16:33:33 +0100

    commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,10 +6,10 @@ class Square:
 Updating the class function, size and its value to an integer and also raising a type and value error 
 to validate that size value is an integer before assigning size to the private instance attribute.
 """
-def __init__(self, size = 0):
+def __init__(self, size = 3):
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif size < 3:
             raise ValueError("size must be >=  0")
         else:
             self.__size = size
