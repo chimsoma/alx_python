b3f8df82ba891d506fd73c70671bae7462649dc3
chimsoma <ndechimsoma@gmail.com>
2023-08-06 15:56:23 +0100

    my first commit

---
+++ python-classes/0-square.py
@@ -0,0 +1,10 @@
+"""
+classes project;   
+"""
+class Square:
+    """
+    The class function in this code defines a square by a private instance attribut called size. 
+    The attribute is made private so, the class builder (author) can easily control the type and value of this attribute.
+    """
+    def __init__(self, size):
+        self.__size = size 
