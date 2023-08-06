fe55d97be1e309ae2d30792eece1cd594ecdf778
chimsoma <ndechimsoma@gmail.com>
2023-08-06 16:30:07 +0100

    commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,9 @@
-
+"""
+classes project;
+"""
 class Square:
  """
-Updating the size value to an integer and also raising a type and value error 
+Updating the class function, size and its value to an integer and also raising a type and value error 
 to validate that size value is an integer before assigning size to the private instance attribute.
 """
 def __init__(self, size = 0):
