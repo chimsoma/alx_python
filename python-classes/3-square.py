c76242a8f877d88f9f35c084c9d35ba2d49083c5
chimsoma <ndechimsoma@gmail.com>
2023-08-07 13:34:03 +0100

    commit

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -8,6 +8,18 @@ In the property decorator a function (retrival method) was used which takes a si
 In the setter decorator(size), which takes two values(self, value). value=value that'll be passed in.
 A type and value error was raised and a public instance method which returns the value of a square.
 """
+    def __init__(self, size = 0):
+        self.__size = size
+        if type(size) is not int:
+                    raise TypeError("size must be an integer")
+        elif size < 0:
+                    raise ValueError("size must be >= 0")
+        else:
+                self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+    
 def __init__(self, size = 0):
     self.__size = size
 
--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,6 +6,37 @@ class Square:
     In this code the public instance method (def my_print(self):)
     was used which prints which prints in stdout the square with the character #.
     """
+    def __init__(self, size = 0):
+        self.__size = size
+        if type(size) is not int:
+                    raise TypeError("size must be an integer")
+        elif size < 0:
+                    raise ValueError("size must be >= 0")
+        else:
+                self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+    
+def __init__(self, size = 0):
+    self.__size = size
+
+@property
+def size(self):
+    return self.__size
+
+@size.setter
+def size(self, value):
+    if type(value) is not int:
+        raise TypeError("size must be an integer")
+    elif value < 0:
+        raise ValueError("size must be >= 0")
+    else:
+        self.__size = value
+    
+def area(self):
+    return self.__size ** 2
+
 def my_print(self):
     if self.__size == 0:
         print()
