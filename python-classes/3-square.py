2f91b9c367fc6e41445bab85faf2a5b8f38f6455
chimsoma <ndechimsoma@gmail.com>
2023-08-07 14:03:20 +0100

    commit

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -16,11 +16,6 @@ def __init__(self, size = 0):
             raise ValueError("size must be >= 0")
         else:
                 self.__size = size
-
-def area(self):
-        return self.__size * self.__size
-    
-
 @property
 def size(self):
     return self.__size
