b94816a5b73cf3fa4608dc8a022f3e4b951a36f7
chimsoma <ndechimsoma@gmail.com>
2023-08-07 13:57:49 +0100

    commit

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -10,9 +10,9 @@ In here we defined a public instance method that returns the current square area
     def __init__(self, size = 0):
         self.__size = size
         if type(size) is not int:
-                    raise TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
-                    raise ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         else:
                 self.__size = size
 
--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -9,10 +9,19 @@ In the setter decorator(size), which takes two values(self, value). value=value 
 A type and value error was raised and a public instance method which returns the value of a square.
 """
 def __init__(self, size = 0):
-    self.__size = size
-    
-    def area(self):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+                self.__size = size
+
+def area(self):
         return self.__size * self.__size
+
+def __init__(self, size = 0):
+    self.__size = size
 
 @property
 def size(self):
